65e25c1fd214f2e2ace6552c85c225cf59e5922f
calvin <calvin.francis96@outlook.com>
2021-10-02 16:48:33 -0400

    Create Home Page component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <HomePage />
       </div>
     </Router>
   );
