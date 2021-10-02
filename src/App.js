bb4f47d54023bb2b8013a4fee4bfe8132904f7fa
calvin <calvin.francis96@outlook.com>
2021-10-02 15:58:00 -0400

    Enable routing in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </Router>
   );
 }
 
