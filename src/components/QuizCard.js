5609847d93a4dcd4c61f0a6df36f2f9eac354bc4
calvin <calvin.francis96@outlook.com>
2021-10-02 17:04:03 -0400

    Add components for quiz index page

---
+++ src/components/QuizCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuizCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default QuizCard;
---
+++ src/components/QuizIndexPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuizIndexPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default QuizIndexPage;
