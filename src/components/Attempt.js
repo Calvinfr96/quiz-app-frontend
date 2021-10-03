66b908884941deb099f979ca95955d6dd546cf67
calvin <calvin.francis96@outlook.com>
2021-10-03 10:35:39 -0400

    Add ability to take quiz when Attempt link is clicked

--- src/components/Attempt.js
+++ src/components/Attempt.js
@@ -3,8 +3,15 @@ import { useParams } from 'react-router-dom';
 import Question from './Question';
 
 function Attempt({baseURL}) {
-    const [quiz, setQuiz] = useState({})
+    const [quiz, setQuiz] = useState({
+        name: "",
+        questions: []
+    })
+    const [questionIndex, setQuestionIndex] = useState(0)
+    const [score, setScore] = useState(0)
     const {id} = useParams()
+    const questions = quiz.questions
+    const currentQuestion = questions[questionIndex]
 
     const fetchQuiz = async () => {
         const data = await fetch(`${baseURL}/quizzes/${id}`)
@@ -16,9 +23,27 @@ function Attempt({baseURL}) {
         fetchQuiz()
     },[])
 
+    function answerQuestion(correct) {
+        if (questionIndex < questions.length) {
+            setQuestionIndex(questionIndex => questionIndex + 1)
+        } else {
+            setQuestionIndex(null)
+        }
+        if (correct) {
+            setScore(score => score + 1)
+        }
+    }
+
     return (
         <div>
-            <h1>Attempt Page</h1>
+            {currentQuestion ? (
+                <Question title={quiz.name} question={currentQuestion} answerQuestion={answerQuestion} />
+            ) : (
+                <div>
+                    <h1>Quiz Finished!</h1>
+                    <h2>Score: {score}</h2>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
-function Question() {
+function Question({title, question, answerQuestion}) {
+    function handleAsnwer(isCorrect, index) {
+        answerQuestion(isCorrect)
+    }
+    const answers = question.answers.map((answer, index) => {
+        const isCorrect = index === question.correct_index
+        return <button key={answer} onClick={() => handleAsnwer(isCorrect)}>{answer}</button>
+    })
     return (
         <div>
+            <h1>{title}</h1>
+            <h2>{question.prompt}</h2>
+            {answers}
         </div>
     )
 }
