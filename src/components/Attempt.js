385dfb77700f8e9774f126180ad98f882a58b63b
calvin <calvin.francis96@outlook.com>
2021-10-02 23:28:10 -0400

    Add Attempt and Question components

---
+++ src/components/Attempt.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import Question from './Question';
+
+function Attempt() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Attempt;
---
+++ src/components/Question.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+
+function Question() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Question;
