24910ae51d929e6a30b997e166a1280efc23dd90
calvin <calvin.francis96@outlook.com>
2021-10-10 11:59:46 -0400

    Style Home Page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div>
-            <p>Welcome to QuizApp!</p>
+        <div className="home-page">
+            <div className="home-page-text">
+                <p>Welcome to Quivia! The app where you can take quizzes on trivia from various subjects.</p>
+                <p>This app was built using a React.js frontend and a Ruby on Rails backend.</p>
+                <p>The app is currently loaded with Computer Trivia questions from Open Trivia Database API.</p>
+                <p className="login-prompt">Please log in or create an account to take a quiz</p>
+            </div>
         </div>
     )
 }
